45ce1ef83e90c3da52f40fcb0b9fb3ae59986891
AAnand05 <44912634+avana1164@users.noreply.github.com>
2024-11-24 11:07:39 -0500

    Fixed headless issue by implementing a user agent

--- scrapers/past_scraper.py
+++ scrapers/past_scraper.py
@@ -7,16 +7,18 @@ url = "https://www.nba.com/stats/teams/advanced?Season=2023-24"
 
 chrome_options = Options()
 chrome_options.add_argument("--enable-unsafe-swiftshader")
-chrome_options.add_argument("--headless")  # Run browser in headless mode
-# chrome_options.add_argument("--disable-gpu")  # Disable GPU acceleration (useful for headless)
-# chrome_options.add_argument("--no-sandbox")  # Avoid sandbox issues (optional)
-# chrome_options.add_argument("--disable-dev-shm-usage")  # Overcome resource limitations (optional)
+chrome_options.add_argument("--headless=new")  # New headless mode
+chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.5735.198 Safari/537.36")
+chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--disable-blink-features=AutomationControlled")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
 
 # setup driver and connect it to url
 service = Service("../../chromedriver-win64/chromedriver.exe")
 driver = webdriver.Chrome(service=service, options=chrome_options)
 driver.get(url)
-driver.implicitly_wait(10)
+driver.implicitly_wait(20)
 
 stats = driver.find_element(By.CSS_SELECTOR, "tbody.Crom_body__UYOcU")
 print(stats.text)
